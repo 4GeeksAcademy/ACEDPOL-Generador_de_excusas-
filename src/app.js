575328536b772a455fb002574493708c25eea82b
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-08 14:13:38 +0000

    First commit: exercise completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var excuseElement;
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  excuseElement = document.getElementById("excuse");
+  generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let a = Math.floor(Math.random() * who.length);
+  let b = Math.floor(Math.random() * action.length);
+  let c = Math.floor(Math.random() * what.length);
+  let d = Math.floor(Math.random() * when.length);
+
+  let excuse = who[a] + " " + action[b] + " " + what[c] + " " + when[d];
+  excuseElement.innerHTML = excuse;
+}
